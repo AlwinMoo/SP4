22909562f52d8db6595c29bf4bde4662b1d96fb9
Alwin <geraldcheu@gmail.com>
2019-02-10 17:10:52 +0800

    Including boilerplate code

---
+++ SP4_unity/Assets/Source/SceneChanger.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    private static SceneChanger instance = new SceneChanger();
+
+    private SceneChanger() { }
+
+    public static SceneChanger GetInstance()
+    {
+        return instance;
+    }
+
+	public void SceneCharSel()
+    {
+        Camera.allCameras[0].Reset();
+        SceneManager.LoadScene("CharSel", LoadSceneMode.Single);
+    }
+
+    public void SceneMain()
+    {
+        SceneManager.LoadScene("Main Game", LoadSceneMode.Single);
+    }
+
+    public void SceneCharCreation()
+    {
+        SceneManager.LoadScene("NewChar", LoadSceneMode.Single);
+    }
+
+    public void SceneSplashscreen()
+    {
+        SceneManager.LoadScene("SplashScreen", LoadSceneMode.Single);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
