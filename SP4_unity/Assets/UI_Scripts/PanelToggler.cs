e21cb9faf640b996e35c2ce385e8f32dfb4b8115
Bigaboyzs <wongzhengyu@gmail.com>
2019-02-11 10:13:12 +0800

    Mainmenu

---
+++ SP4_unity/Assets/UI_Scripts/PanelToggler.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelToggler : MonoBehaviour {
+
+    public GameObject OpenPanel;
+    public GameObject ClosePanel;
+
+    public void PanelOpen() 
+    {
+        if(OpenPanel != null)
+        {
+            OpenPanel.SetActive(true);
+        }
+    }
+
+    public void PanelClose()
+    {
+        if(ClosePanel != null)
+        {
+            ClosePanel.SetActive(false);
+        }
+    }
+}
