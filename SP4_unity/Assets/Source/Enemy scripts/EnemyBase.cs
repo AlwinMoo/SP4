d4ccf3021886be3f4aa2824d459d737b8d942e0f
Alwin <geraldcheu@gmail.com>
2019-02-11 10:54:25 +0800

    Created virtual enemy base and moved some files around

---
+++ SP4_unity/Assets/Source/Enemy scripts/EnemyBase.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBase : MonoBehaviour{
+
+    public float health { get; set; }
+    public float mass { get; set; }
+
+    // Use this for initialization
+    public virtual void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    public virtual void Update()
+    {
+
+    }
+}
