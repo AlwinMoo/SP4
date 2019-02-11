086557a263a55509c6ae610b0b065a61c94bcb34
Bigaboyzs <wongzhengyu@gmail.com>
2019-02-11 14:26:16 +0800

    Fog of war

--- SP4_unity/Assets/Source/Movement.cs
+++ SP4_unity/Assets/Source/Movement.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour {
 
+    public float moveSpeed = 10;
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +12,6 @@ public class Movement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        transform.Translate(Input.GetAxis("Horizontal") * Time.deltaTime, 0.0f, Input.GetAxis("Vertical") * Time.deltaTime);
+        transform.Translate(Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed, 0.0f, Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed);
 	}
 }
