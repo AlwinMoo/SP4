549c2d1de43d295123dce4b88bed63d16fb97c88
Alwin <geraldcheu@gmail.com>
2019-02-11 17:35:18 +0800

    Collision prep incomplete

--- SP4_unity/Assets/Source/Enemy scripts/EnemyBase.cs
+++ SP4_unity/Assets/Source/Enemy scripts/EnemyBase.cs
@@ -1,13 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyBase : MonoBehaviour{
 
     public float health { get; set; }
     public float mass { get; set; }
 
+    GameObject target;
+    NavMeshAgent agent;
+
+    private float m_countDown;
+    private const float cooldown = 3.0f;
+
     // Use this for initialization
+    public virtual void Awake()
+    {
+        m_countDown = cooldown;
+        agent = this.GetComponent<NavMeshAgent>();
+        target = GameObject.FindGameObjectWithTag("Player");
+    }
+
     public virtual void Start()
     {
 
@@ -16,6 +30,18 @@ public class EnemyBase : MonoBehaviour{
     // Update is called once per frame
     public virtual void Update()
     {
+        m_countDown -= Time.deltaTime;
 
+        if (m_countDown <= 0.0f)
+        {
+            m_countDown += cooldown;
+            agent.SetDestination(target.transform.position);
+            Debug.Log("updated destination");
+        }
+    }
+
+    public virtual void OnCollisionEnter(Collision collision)
+    {
+        
     }
 }
