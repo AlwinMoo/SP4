a3a77de92cad3548cec612fc082ccb64fdb56105
Alwin <geraldcheu@gmail.com>
2019-02-11 10:11:04 +0800

    Added basic car class for now
    Not fleshed out, basic car class

---
+++ SP4_unity/Assets/Source/Sedan.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sedan : VehicleParent
+{
+	// Use this for initialization
+	public override void Start () {
+        health = 100;
+        mass = 1000;
+	}
+
+    // Update is called once per frame
+    public override void Update () {
+		
+	}
+}
