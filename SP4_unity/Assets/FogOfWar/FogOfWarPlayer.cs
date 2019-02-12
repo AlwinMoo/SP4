35f192d154702e45d34e8662ac376694e80e6c8f
Chew Jia Le <jiale.89@hotmail.com>
2019-02-12 10:58:00 +0800

    Update FogOfWarPlayer.cs

--- SP4_unity/Assets/FogOfWar/FogOfWarPlayer.cs
+++ SP4_unity/Assets/FogOfWar/FogOfWarPlayer.cs
@@ -2,34 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FogOfWarPlayer : MonoBehaviour {
+public class FogOfWarPlayer : MonoBehaviour
+{
 
-    public Transform FogOfWarPlane;
     public GameObject FogPlane;
     public int Number;
 
     // Use this for initialization
-    void Start ()
+    void Start()
     {
-        FindFogPlane();
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update()
+    {
         Vector3 screenPos = Camera.main.WorldToScreenPoint(transform.position);
         Ray rayToPlayerPos = Camera.main.ScreenPointToRay(screenPos);
 
         RaycastHit hit;
-        if(Physics.Raycast(rayToPlayerPos, out hit, 1000))
+        if (Physics.Raycast(rayToPlayerPos, out hit, 1000))
         {
-            FogOfWarPlane.GetComponent<Renderer>().material.SetVector("Player" + Number.ToString(), hit.point);
+            FindFogPlane().GetComponent<Renderer>().material.SetVector("Player" + Number.ToString(), hit.point);
         }
-	}
+    }
 
-    void FindFogPlane()
+    Transform FindFogPlane()
     {
         FogPlane = GameObject.Find("FogOfWarPlane");
         Debug.Log("Found FogPlane");
+        return FogPlane.transform;
     }
 
 }
