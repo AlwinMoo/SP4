a2ede383c040080f19c43669fa852bb973b93a38
Bigaboyzs <wongzhengyu@gmail.com>
2019-02-12 09:38:58 +0800

    Cameras

---
+++ SP4_unity/Assets/Source/thirdPerCam.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class thirdPerCam : MonoBehaviour {
+
+    public Transform PlayerTransform;
+
+    private Vector3 _cameraOffset;
+
+    public float SmoothFac = 0.5f;
+
+    public bool LookAtPlayer = false;
+
+    public bool RotateAroundPlayer = true;
+
+    public float RotationSpeed = 5.0f;
+
+    void Start()
+    {
+        _cameraOffset = transform.position - PlayerTransform.position;
+    }
+
+    void LateUpdate()
+    {
+
+        if(RotateAroundPlayer)
+        {
+            Quaternion camTurnAngle = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * RotationSpeed, Vector3.up);
+            _cameraOffset = camTurnAngle * _cameraOffset;
+        }
+        Vector3 newPos = PlayerTransform.position + _cameraOffset;
+
+        transform.position = Vector3.Slerp(transform.position, newPos, SmoothFac);
+
+        if(LookAtPlayer || RotateAroundPlayer)
+        {
+            transform.LookAt(PlayerTransform);
+        }
+
+    }
+
+}
