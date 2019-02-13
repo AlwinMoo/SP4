a727f066abb52ae60c1923d4936d77afde20ae5b
Bigaboyzs <wongzhengyu@gmail.com>
2019-02-13 17:48:37 +0800

    choosing car to go to the mainscene

--- SP4_unity/Assets/Source/PersistantData.cs
+++ SP4_unity/Assets/Source/PersistantData.cs
@@ -12,11 +12,12 @@ public class PersistantData : MonoBehaviour
         // Do not use MonoBehaviour class to save
         BinaryFormatter BF = new BinaryFormatter();
         // Application.persistentDataPath(where you are saving to) // "/SaveFile.dat" is the file name
-        FileStream file = File.Create(Application.persistentDataPath + "/SoundFile.dat");
+        FileStream file = File.Create(Application.persistentDataPath + "/DataFile.dat");
 
-        SoundData data = new SoundData();
+        Data data = new Data();
         data.BGMvolume = BGM.BGMvolchanger.audioSrc.volume;
         data.SFXvolume = SFX.SFXvolchanger.audioSrc.volume;
+        data.SelectedvehicleID = SliderValue.ID;
 
         Debug.Log("BGMSaveData" + data.BGMvolume);
 
@@ -29,18 +30,19 @@ public class PersistantData : MonoBehaviour
     public void Load()
     {
         // Check if the file is there
-        if(File.Exists(Application.persistentDataPath + "/SoundFile.dat"))
+        if(File.Exists(Application.persistentDataPath + "/DataFile.dat"))
         {
             BinaryFormatter BF = new BinaryFormatter();
             // opens the file
-            FileStream file = File.Open(Application.persistentDataPath + "/SoundFile.dat", FileMode.Open);
-            SoundData data = (SoundData)BF.Deserialize(file);
+            FileStream file = File.Open(Application.persistentDataPath + "/DataFile.dat", FileMode.Open);
+            Data data = (Data)BF.Deserialize(file);
             file.Close();
 
             BGM.BGMvolchanger.audioSrc.volume = data.BGMvolume;
             SFX.SFXvolchanger.audioSrc.volume = data.SFXvolume;
             BGMSlider.BGMslid.BGMslider.value = data.BGMvolume;
             SFXSlider.SFXSlid.SFXslider.value = data.SFXvolume;
+            PlayersCar.PlayerscarID = data.SelectedvehicleID;
 
 
             Debug.Log("BGMLoadedData" + data.BGMvolume);
@@ -51,8 +53,9 @@ public class PersistantData : MonoBehaviour
 
 // [Serializable] is needed to tell unity that this class can be saved
 [Serializable]
-class SoundData
+class Data
 {
     public float SFXvolume;
     public float BGMvolume;
+    public int SelectedvehicleID;
 }
---
+++ SP4_unity/Assets/Source/PlayersCar.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayersCar : MonoBehaviour {
+
+    public static int PlayerscarID;
+    GameObject sedan;
+    GameObject van;
+
+	void Update () {
+
+        switch (PlayerscarID)
+        {
+            case 1:
+                {
+                    if(van != null)
+                    {
+                        van.SetActive(true);
+                    }
+                    sedan.SetActive(true);
+                    break;
+                }
+            case 2:
+                {
+                    if (sedan != null)
+                    {
+                        sedan.SetActive(true);
+                    }
+                    van.SetActive(true);
+                    break;
+                }
+        }
+    }
+}
