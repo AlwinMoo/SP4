1d05e4324b8a8177e679b88383e4fe1b1fddc6cf
Alonzo Nalpon <judgementbearmaurice@gmail.com>
2019-02-13 11:27:23 +0800

    Flamethrower with flaming objects and flaming scene added

---
+++ SP4_unity/Assets/Source/Weapon_Scripts/Fire.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fire : MonoBehaviour {
+
+	void OnParticleCollision(GameObject other)
+	{
+		Flammable objectOnFire = other.GetComponent<Flammable> ();
+		if (objectOnFire != null) 
+		{
+			// Function returns a bool if the enemy was ignited
+			objectOnFire.Ignited ();
+		}
+	}
+}
