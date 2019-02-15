60a87046e4ff7066253467be497fa1ad911d2fb0
Alonzo Nalpon <judgementbearmaurice@gmail.com>
2019-02-15 12:38:52 +0800

    Added forge and 2 example scenes

---
+++ SP4_unity/Assets/MoveCube.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BeardedManStudios.Forge.Networking.Generated;
+
+public class MoveCube : MoveCubeBehavior{
+
+	private void Update()
+	{
+		if (!networkObject.IsServer) {
+			transform.position = networkObject.position;
+			transform.rotation = networkObject.rotation;
+			return;
+		}
+		transform.position += new Vector3 (
+			Input.GetAxis ("Horizontal"),
+			Input.GetAxis ("Vertical"),
+			0
+		) * Time.deltaTime * 5.0f;
+		transform.Rotate (transform.up, Time.deltaTime * 90.0f);
+		// Assign the transform information here
+		networkObject.position = transform.position;
+		networkObject.rotation = transform.rotation;
+	}
+}
