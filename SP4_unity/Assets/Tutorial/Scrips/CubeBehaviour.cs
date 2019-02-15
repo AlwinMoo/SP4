bdf7d78e47f4aaf34987c1013ff595de8f7140f8
Alonzo Nalpon <judgementbearmaurice@gmail.com>
2019-02-15 10:16:30 +0800

    Added 2 cube demo with movement but no interpolation

---
+++ SP4_unity/Assets/Tutorial/Scrips/CubeBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+//using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeBehaviour : Bolt.EntityBehaviour<ICubeState> {
+
+	// Like Start, but only called after object exists in network
+	public override void Attached()
+	{
+		state.SetTransforms (state.CubeTransform, transform);
+	}
+	
+	public override void SimulateOwner()
+	{
+		var speed = 4f;
+		var movement = Vector3.zero;
+
+		if (Input.GetKey (KeyCode.W)) { movement.z += 1; }
+		if (Input.GetKey(KeyCode.S)) { movement.z -= 1; }
+		if (Input.GetKey(KeyCode.A)) { movement.x -= 1; }
+		if (Input.GetKey(KeyCode.D)) { movement.x += 1; }
+
+		// Avoid divide by zero
+		if (movement != Vector3.zero)
+		{
+			transform.position = transform.position + (movement.normalized * speed * BoltNetwork.frameDeltaTime);
+		}
+	}
+}
---
+++ SP4_unity/Assets/Tutorial/Scrips/NetworkCallbacks.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+//using System.Collections.Generic;
+using UnityEngine;
+
+[BoltGlobalBehaviour]
+public class NetworkCallbacks : Bolt.GlobalEventListener {
+	public override void SceneLoadLocalDone(string map)
+	{
+		// Randomize a position
+		var spawnPosition = new Vector3(Random.Range(-8, 8), 0, Random.Range(-8, 8));
+
+		// instantiate cube
+		BoltNetwork.Instantiate(BoltPrefabs.Cube, spawnPosition, Quaternion.identity);
+	}
+
+}
