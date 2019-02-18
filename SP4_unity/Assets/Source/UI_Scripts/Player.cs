0d8e67dbdeee78c53ce9b1298565d269b0e8abb7
Bigaboyzs <wongzhengyu@gmail.com>
2019-02-18 14:38:01 +0800

    Multiplayer lobby

---
+++ SP4_unity/Assets/Source/UI_Scripts/Player.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour {
+
+    public Text Name;
+    public Text Car;    
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
