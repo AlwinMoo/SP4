a9aadfb4e8d9a02cdaa5122ffc126257fa45e0bd
Bigaboyzs <wongzhengyu@gmail.com>
2019-02-18 17:39:33 +0800

    jlkdshuikvgfdhjkafd

--- SP4_unity/Assets/Source/UI_Scripts/ReadyToggler.cs
+++ SP4_unity/Assets/Source/UI_Scripts/ReadyToggler.cs
@@ -2,10 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using BeardedManStudios.Forge.Networking.Unity;
+using UnityEngine.SceneManagement;
 
 public class ReadyToggler : MonoBehaviour {
 
-    public Toggle ReadyToggle;
+    private GameObject PlayerToggle;
+    private Toggle ReadyToggle;
+
+    void Start()
+    {
+        PlayerToggle = GameObject.FindWithTag("Player"); // + put the players id in here);
+        ReadyToggle = PlayerToggle.GetComponent<Toggle>();
+    }
 
 	public void ToggleOnOff()
     {
@@ -18,6 +27,14 @@ public class ReadyToggler : MonoBehaviour {
         {
             Debug.Log("Ready");
             ReadyToggle.isOn = true;
+        }
+    }
+
+    public void changeScene()
+    {
+        if (NetworkManager.Instance.Networker.IsServer)
+        {
+            SceneManager.LoadScene(2, LoadSceneMode.Single);
         }
     }
 }
