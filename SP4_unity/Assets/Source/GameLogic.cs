4068960475c82480ee1ff22de6273c7d63aee79d
Alwin <geraldcheu@gmail.com>
2019-02-19 14:33:28 +0800

    Revert "hotfix"
    This reverts commit 16c1e397f6607b0f4c0a7006d21d14cde3ecc969.

--- SP4_unity/Assets/Source/GameLogic.cs
+++ SP4_unity/Assets/Source/GameLogic.cs
@@ -11,19 +11,15 @@ public class GameLogic : MonoBehaviour
     void Start ()
     {
         Random.InitState((int)System.DateTime.Now.Ticks);
-
-        if (PlayerManager.playerManager.networkObject.IsServer)
+        for (int i = 0; i < PlayerManager.playerManager.m_playerCount; ++i)
         {
-            for (int i = 0; i < PlayerManager.playerManager.m_playerCount; ++i)
-            {
-                Vector3 randpos = new Vector3(Random.Range(0, 20), 0, Random.Range(0, 20));
-                var newCar = NetworkManager.Instance.InstantiatePlayerVehicle(PlayerManager.playerManager.m_players[i].player_car, randpos, transform.rotation, true);
+            Vector3 randpos = new Vector3(Random.Range(0, 20), 0, Random.Range(0, 20));
+            var newCar = NetworkManager.Instance.InstantiatePlayerVehicle(PlayerManager.playerManager.m_players[i].player_car, randpos, transform.rotation, true);
 
-                if (PlayerManager.playerManager.m_players[i].player_ID == 0)
-                {
-                    newCar.gameObject.AddComponent<Camera>();
-                    newCar.gameObject.GetComponent<Camera>().gameObject.AddComponent<CameraFollow>();
-                }
+            if (PlayerManager.playerManager.m_players[i].player_ID == 0)
+            {
+                newCar.gameObject.AddComponent<Camera>();
+                newCar.gameObject.GetComponent<Camera>().gameObject.AddComponent<CameraFollow>();
             }
         }
     }
