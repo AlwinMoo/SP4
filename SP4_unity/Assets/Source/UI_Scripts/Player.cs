c0246a3c4b7135db1016c8f1a85f1c9867743d3a
Bigaboyzs <wongzhengyu@gmail.com>
2019-02-20 15:38:03 +0800

    multiplayer_Lobby

--- SP4_unity/Assets/Source/UI_Scripts/Player.cs
+++ SP4_unity/Assets/Source/UI_Scripts/Player.cs
@@ -7,10 +7,12 @@ public class Player : MonoBehaviour {
 
     public Text Name;
     public Text Car;
+    public Toggle PlayerToggle;
     public int slotNumber;
 
 	// Update is called once per frame
 	void Update () {
         Name.text = PlayerManager.playerManager.GetPlayerName(slotNumber);
-	}
+        
+    }
 }
