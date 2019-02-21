0ce3cb39d18bef6d755aedb74370a9cd5981ca57
Chew Jia Le <jiale.89@hotmail.com>
2019-02-21 19:58:59 +0800

    fasdghk

---
+++ SP4_unity/Assets/Source/RotationLerp.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationLerp : MonoBehaviour
+{
+    Vector3 directionVector;
+    Quaternion targetRotation;
+
+    private Transform target;
+    public float speed = 0.1f;
+
+    //bool rotating = false;
+
+    //float rotaionTime = 0;
+
+    // Update is called once per frame
+    void Update ()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Plane plane = new Plane(Vector3.up, this.transform.position);
+        float distToPlane;
+        //target.position = Input.mousePosition;
+        if (plane.Raycast(ray, out distToPlane))
+        {
+            Vector3 hitPos = ray.GetPoint(distToPlane);
+
+            Vector3 dir = hitPos - transform.position;
+            dir.y = 0;
+
+            targetRotation = Quaternion.LookRotation(dir);
+            transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, 1.0f);
+        }
+        
+	}
+}
